8311872f969a9e7f18d76b06b361d6b54bccdbd7
dmitry.miroshnichenko <dmitry.miroshnichenko@arcadia.spb.ru>
2022-02-14 13:28:20 +0300

    Add solution (2)

---
+++ AT_Training/Exercise_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AT_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            bool repeat = true;
+            while (repeat)
+            {
+                do
+                {
+                    Console.Write("A = ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out a));
+                do
+                {
+                    Console.Write("B = ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out b));
+                if (a <= 0 || b <= 0)
+                {
+                    Console.WriteLine("Ошибка, попробовать еще раз? Y/N");
+                    var ki = Console.ReadKey(true);
+                    if (ki.Key != ConsoleKey.Y)
+                        repeat = false;
+                    continue;
+                }
+                Console.WriteLine("A: {0}, B: {1}, SSSS: {2}", a, b, a * b);
+                repeat = false;
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ AT_Training/Exercise_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("length: ");
+            double length = int.Parse(Console.ReadLine());
+            Console.Write("width: ");
+            double width = int.Parse(Console.ReadLine());
+            Console.Write("height: ");
+            double height = int.Parse(Console.ReadLine());
+
+            double diagonal = Math.Sqrt (length + width + height);
+
+            Console.WriteLine("diagonal: {0}", diagonal);
+            Console.ReadLine();
+        }
+    }
+}
