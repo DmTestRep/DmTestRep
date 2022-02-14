f0577f785b1d0826694f60101ca3d2e16bbd4509
dmitry.miroshnichenko <dmitry.miroshnichenko@arcadia.spb.ru>
2022-02-14 13:50:22 +0300

    123

--- AT_Training/Exercise_2/Program.cs
+++ AT_Training/Exercise_2/Program.cs
@@ -7,13 +7,13 @@ namespace Exercise_2
         static void Main(string[] args)
         {
             Console.Write("length: ");
-            double length = int.Parse(Console.ReadLine());
+            double length = Convert.ToDouble(Console.ReadLine());
             Console.Write("width: ");
-            double width = int.Parse(Console.ReadLine());
+            double width = Convert.ToDouble(Console.ReadLine());
             Console.Write("height: ");
-            double height = int.Parse(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
 
-            double diagonal = Math.Sqrt (length + width + height);
+            double diagonal = Math.Sqrt (Math.Pow(length, 2) + Math.Pow(width, 2) + Math.Pow(height, 2));
 
             Console.WriteLine("diagonal: {0}", diagonal);
             Console.ReadLine();
